fc215f49d1137d96f4da3459cf112516d954584e
kambe.kenta <kenta.k199405@gmail.com>
2020-11-08 21:17:39 +0900

    using data from db

--- assets/database.py
+++ assets/database.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 import datetime
 import os
+import pandas as pd
 
 databese_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data.db')
 engine = create_engine('sqlite:///' + databese_file, convert_unicode=True , echo=True)
@@ -21,3 +22,12 @@ Base.query = db_session.query_property()
 def init_db():
     import assets.models
     Base.metadata.create_all(bind=engine)
+
+def read_data():
+    from assets import models
+    df = pd.read_csv('assets/data.csv')
+    for index, _df in df.iterrows():
+        date = datetime.datetime.strptime(_df['date'],'%Y/%m/%d').date()
+        row = models.Data(date=date, subscribers=_df['subscribers'],reviews=_df['reviews'])
+        db_session.add(row)
+    db_session.commit()
--- view.py
+++ view.py
@@ -4,21 +4,24 @@ import dash_html_components as html
 import plotly.graph_objs as go
 import pandas as pd
 import datetime
+from assets.database import db_session
+from assets.models import Data
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-df = pd.read_csv('assets/data.csv')
+data = db_session.query(Data.date, Data.subscribers, Data.reviews).all()
 
 dates = []
-for _date in df['date']:
-    date = datetime.datetime.strptime(_date, '%Y/%m/%d').date()
-    dates.append(date)
+subscribers = []
+reviews = []
 
-n_subscribers = df['subscribers'].values
-n_reviews = df['reviews'].values
+for datum in data:
+    dates.append(datum.date)
+    subscribers.append(datum.subscribers)
+    reviews.append(datum.reviews)
 
-diff_subscribers = df['subscribers'].diff().values
-diff_reviews = df['reviews'].diff().values
+diff_subscribers = pd.Series(subscribers).diff().values
+diff_reviews = pd.Series(reviews).diff().values
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
@@ -31,7 +34,7 @@ app.layout = html.Div(children=[
                 'data':[
                     go.Scatter(
                         x=dates,
-                        y=n_subscribers,
+                        y=subscribers,
                         mode='lines+markers',
                         name='Number of students',
                         opacity=0.7,
@@ -48,7 +51,7 @@ app.layout = html.Div(children=[
                     title='Transition number of students',
                     xaxis=dict(title='date'),
                     yaxis=dict(title='Number of students', side='left', showgrid=False,
-                    range=[2000, max(n_subscribers)+100]),
+                    range=[2000, max(subscribers)+100]),
                     yaxis2=dict(title='Increased number of students', side='right', overlaying='y', showgrid=False,
                     range=[0, max(diff_subscribers[1:])]),
                     margin=dict(l=200, r=200, b=100, t=100)
@@ -61,7 +64,7 @@ app.layout = html.Div(children=[
                 'data':[
                     go.Scatter(
                         x=dates,
-                        y=n_reviews,
+                        y=reviews,
                         mode='lines+markers',
                         name='Number of reviews',
                         opacity=0.7,
@@ -78,7 +81,7 @@ app.layout = html.Div(children=[
                     title='Transition number of reviews',
                     xaxis=dict(title='date'),
                     yaxis=dict(title='Number of reviews', side='left', showgrid=False,
-                    range=[0, max(n_reviews)+10]),
+                    range=[0, max(reviews)+10]),
                     yaxis2=dict(title='Increased number of reviews', side='right', overlaying='y', showgrid=False,
                     range=[0, max(diff_reviews[1:])]),
                     margin=dict(l=200, r=200, b=100, t=100)
